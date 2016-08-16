534d27332071f7ba87a8ceadedd8ee3272c925a1
Jonas Westlund <jonve547@student.liu.se>
2016-08-16 23:51:36 +0200

    Uppdaterat dotfiles

--- install.sh
+++ install.sh
@@ -6,11 +6,3 @@ sudo apt-get update
 sudo apt-get install i3
 cp -r i3/ ~/.config/
 
-# Install and configure Emacs
-sudo apt-get build-dept emacs24
-curl -# "http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.gz"
-tar -xvzf emacs-24.5.tar.gz
-cd emacs-24.5
-./configure
-make
-sudo make install
