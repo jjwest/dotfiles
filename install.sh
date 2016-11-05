4a418425edbd5442ee3377883fc5151c4dd64635
Jonas Vestlund <jonve547@student.liu.se>
2016-11-05 22:23:26 +0100

    Rensat

--- install.sh
+++
@@ -1,8 +0,0 @@
-# Install and configure i3wm
-sudo echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get --allow-unauthenticated install sur5r-keyring
-sudo apt-get update
-sudo apt-get install i3
-cp -r i3/ ~/.config/
-
