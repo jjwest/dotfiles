d041b2696b5f8ae8ac43e69b055fed2abddc42b8
Jonas Westlund <jonaswestlund101@gmail.com>
2017-10-16 02:50:10 +0200

    Updated gitconfig

--- git-setup.sh
+++ git-setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 cp .gitignore_global $HOME
+git config --global pull.rebase true
+git config --global user.name "Jonas Westlund"
+git config --global user.email "jonaswestlund101@gmail.com"
 git config --global core.excludesfile $HOME/.gitignore_global
